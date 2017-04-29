271b4e59b32b297a3fb6bbfa42ec562c25a24e9b
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-04-29 15:32:12 -0400

    finished lab

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+  foo = []
+  if block_given?
+    i = 0
+    while i < collection.length
+      if yield(collection[i])
+        foo << collection[i]
+        i += 1
+      else
+        i += 1
+      end
+    end
+  else
+    puts "no block given :("
+  end
+  foo
 end
